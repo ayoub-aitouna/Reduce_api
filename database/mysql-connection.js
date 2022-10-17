34d928b72638977e3c06f7634833c14699ecf93a
Ayoub Aitouna <aitounaayoub05@gmail.com>
2022-10-17 19:14:37 +0000

    Update 4 files

--- database/mysql-connection.js
+++ database/mysql-connection.js
@@ -4,36 +4,36 @@ var syncSql = require("sync-sql");
 const { BadRequestError } = require("../errors");
 
 const connect = {
-	user: "root",
-	host: "35.233.102.83",
-	password: "I2%DhdQvmI-J.S0-",
-	database: "defaultdb",
-	port: 3306,
-	multipleStatements: true,
+  user: "root",
+  host: "35.195.162.181",
+  password: "fEF6J$&RE|=n<S0r",
+  database: "defaultdb",
+  port: 3306,
+  multipleStatements: true,
 };
 var Mysql = mysql.createConnection(connect);
 
 Mysql.connect(function (err) {
-	if (err) {
-		Log.error(`An Error while trying to connect to databse ==> ${err}`);
-		throw err;
-	}
-	Log.info(`SQL DB Connected`);
+  if (err) {
+    Log.error(`An Error while trying to connect to databse ==> ${err}`);
+    throw err;
+  }
+  Log.info(`SQL DB Connected`);
 });
 
 function Query(query) {
-	try {
-		return syncSql.mysql(connect, query).data.rows;
-	} catch (err) {
-		throw new BadRequestError(err);
-	}
+  try {
+    return syncSql.mysql(connect, query).data.rows;
+  } catch (err) {
+    throw new BadRequestError(err);
+  }
 }
 
 function SqlQuery(query) {
-	try {
-		return syncSql.mysql(connect, query);
-	} catch (err) {
-		throw new BadRequestError(err);
-	}
+  try {
+    return syncSql.mysql(connect, query);
+  } catch (err) {
+    throw new BadRequestError(err);
+  }
 }
 module.exports = { Mysql, Query, SqlQuery };

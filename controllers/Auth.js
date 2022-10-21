43741aab5c1de67f29dccb68b885415cda1399cc
Ayoub Aitouna <aitounaayoub05@gmail.com>
2022-10-21 11:15:06 +0000

    Update 4 files

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,7 +2,6 @@ const { Mysql, Query, SqlQuery } = require("../database/index.js");
 const jwt = require("jsonwebtoken");
 const { client } = require("../database/index.js");
 require("dotenv").config();
-const Log = require("../log");
 const { generateKeyAndstoreOtp } = require("../Utils/OTP.js");
 const { GenrateAvaratByName } = require("../Utils/Avatar");
 

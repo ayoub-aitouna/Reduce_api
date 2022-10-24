2bd6fb48b81633d3ad14122fe1f2df6b0e38367e
Ayoub Aitouna <aitounaayoub05@gmail.com>
2022-10-24 14:59:31 +0000

    Update Pdfgenerator.js

--- Utils/Pdfgenerator.js
+++ Utils/Pdfgenerator.js
@@ -1,17 +1,20 @@
 const pdf = require("html-pdf");
+const { UploadFile, UploadBuffer } = require("../Utils/Files");
+
 const { Pdf_contract_template } = require("./Templates.js");
 const Generate_contract_Pdf = async (partner_data) => {
   const { content } = Pdf_contract_template(partner_data);
   return new Promise((res, rej) => {
     const path = `./public/pdf/contracr_${partner_data.id}_Result.pdf`;
     try {
-      pdf.create(content, {}).toFile(path, (err) => {
+      pdf.create(content, {}).toFile(path, async (err) => {
         if (err) {
           console.log(err);
           rej({ msg: err });
         } else {
-          console.log("OK");
-          res(__dirname + path);
+          const { url, message } = await UploadFile(path);
+          console.trace({ url, message });
+          res(url);
         }
       });
     } catch (err) {

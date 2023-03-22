dbb0741b048dfd9206dc72370cb85888f78673c2
Ayoub Aitouna <aaitouna@e2r1p9.1337.ma>
2023-03-22 23:14:22 +0000

    changes controllers/Activities.js routes/Activities.js

--- controllers/Activities.js
+++ controllers/Activities.js
@@ -4,49 +4,49 @@ const {BadRequestError} = require("../errors/index.js");
 require("dotenv").config();
 
 const AddActivity = async (req, res) => {
-  const { Activity , icon } = req.body;
+	const { Activity , icon } = req.body;
 
-  const added_Activity = SqlQuery(`insert into entrprise_activities(
-    activity_name,
+	const added_Activity = SqlQuery(`insert into entrprise_activities(
+	activity_name,
 	icon,
-    created_date
+	created_date
 	) values (
 		'${Activity}',
 		'${icon}',
 		CURDATE()
 	)`);
-  if (!added_Activity.success)
-    return res.status(500).json({
-      err: added_Activity.data.err,
-    });
+	if (!added_Activity.success)
+		return res.status(500).json({
+			err: added_Activity.data.err,
+		});
 
-  res.status(200).send({
-    msg: `OK`,
-  });
+	res.status(200).send({
+		msg: `OK`,
+	});
 };
 
 const Activities = async (req, res) => {
-  const entrprise_activities = SqlQuery(`select * from entrprise_activities`);
-  if (!entrprise_activities.success)
-    return res.status(500).json({
-      err: entrprise_activities.data.err,
-    });
-  res.status(200).json(entrprise_activities.data.rows);
+	const entrprise_activities = SqlQuery(`select * from entrprise_activities`);
+	if (!entrprise_activities.success)
+		return res.status(500).json({
+			err: entrprise_activities.data.err,
+		});
+	res.status(200).json(entrprise_activities.data.rows);
 };
 
 const get_cities = async (req, res)=>{
 
 	const {id} = req.Query;
-	
+
 	const cities = SqlQuery(`SELECT c.id, c.ville_name, IF(b.activity_id IS NULL, TRUE, FALSE)
-					AS is_blocked FROM villes c
+					AS status FROM villes c
 					LEFT JOIN blocked_activities b
 						ON c.id = b.city_id
 							AND b.activity_id = ${id};`);
 	if(!cities.success)
-	return res.status(500).json({	
-      err: entrprise_activities.data.err,
-	});
+		return res.status(500).json({	
+			err: entrprise_activities.data.err,
+		});
 	res.status(200).json(cities.rows);
 }
 
@@ -60,10 +60,10 @@ const toggle_city = async(req, res)=> {
 		const currentStatus = rows[0].status;
 		const newStatus = !currentStatus;
 		let update = SqlQuery(`UPDATE blocked_activities SET status = ${newStatus} WHERE city_id = ${cityId} AND activity_id = ${activityId}`);
-  }
-  else {
-    conn.query('INSERT INTO blocked_activities (city_id, activity_id, status) VALUES (?, ?, false)', [cityId, activityId]);
-  }
+	}
+	else {
+		conn.query('INSERT INTO blocked_activities (city_id, activity_id, status) VALUES (?, ?, false)', [cityId, activityId]);
+	}
 }
 
 const get_acviity_by_city = async (req, res) =>{
--- routes/Activities.js
+++ routes/Activities.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const { AddActivity, Activities} = require("../controllers/Activities");
 router.post("/Add", AddActivity);
 router.get("/", Activities);
-//router.get("/cities", get_cities);
-/*
+router.get("/cities", get_cities);
 router.get("/get_acviity_by_city", get_acviity_by_city);
 router.post("/toggle_city", toggle_city);
-*/
+
 module.exports = router;
